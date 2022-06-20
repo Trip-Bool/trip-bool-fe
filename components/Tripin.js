08a97b2a9bbbed7ec5be9f7dfc94c97771925619
Cole Gibbs <colegibbs0@gmail.com>
2022-06-20 14:26:16 -0700

    handle trip form

--- components/Tripin.js
+++ components/Tripin.js
@@ -1,6 +1,15 @@
 import CreateTripForm from "./CreateTripForm";
+import { useState } from 'react';
 
 export default function Tripin() {
+
+  const [tripInfo, setTripInfo] = useState({})
+  function handleTripCreation(trip){
+    //trip data from user input may need to be manipulated based on the public api being used
+    tripdata = axios.get();//call out api with user input
+    setTripInfo(tripdata);
+  }
+
   return(
     <CreateTripForm />
   )
