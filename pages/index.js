d5eef9f024019a1032cbc3ac58ed4c4e92a2bfad
Cole Gibbs <colegibbs0@gmail.com>
2022-06-20 11:56:17 -0700

    import Header, Tripin, LoginForm

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
 import Head from 'next/head'
+import Header from 'components/Header'
+import Tripin from 'components/Tripin'
+import LoginForm from 'components/LoginForm'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
@@ -9,8 +12,13 @@ export default function Home() {
       <title>Tripin</title>
     </Head>
 
+    <Header />
+
     {user ?
-      <}
+      <Tripin />
+    :
+      <LoginForm />
+    }
     </>
   )
 }
