806742fd6fd06c1053f3b0f984d3d950146b6b4e
Cole Gibbs <colegibbs0@gmail.com>
2022-06-20 14:47:52 -0700

    changed assumed data coming from the api

--- components/Tripin.js
+++ components/Tripin.js
@@ -3,11 +3,10 @@ import { useState } from 'react';
 
 export default function Tripin() {
 
-  const [tripInfo, setTripInfo] = useState({})
+  const [trips, setTrips] = useState({})
   function handleTripCreation(trip){
-    //trip data from user input may need to be manipulated based on the public api being used
-    // tripdata = axios.get();//call out api with user input
-    setTripInfo(trip);//change trip to tripdata one the get request is working
+    // tripdata = axios.get();//call out api with user input - assuming from this point forward that our api is return a list of trips
+    setTrips(tripdata);
   }
 
   return(
