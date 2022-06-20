0e56f3aa6cbdae999a84e3b0072922d3b597dfd6
Cole Gibbs <colegibbs0@gmail.com>
2022-06-20 14:31:17 -0700

    created function to handle trip form and passed to the form componenet

--- components/Tripin.js
+++ components/Tripin.js
@@ -6,11 +6,13 @@ export default function Tripin() {
   const [tripInfo, setTripInfo] = useState({})
   function handleTripCreation(trip){
     //trip data from user input may need to be manipulated based on the public api being used
-    tripdata = axios.get();//call out api with user input
-    setTripInfo(tripdata);
+    // tripdata = axios.get();//call out api with user input
+    setTripInfo(trip);//change trip to tripdata one the get request is working
   }
 
   return(
-    <CreateTripForm />
+    <>
+    <CreateTripForm handleTripCreation={handleTripCreation}/>
+    </>
   )
 }
