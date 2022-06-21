e773061657ec9592fb066355d2df0e3657b7203d
Jacob Choi <jacob.j.choi@gmail.com>
2022-06-21 14:28:56 -0700

    revised version of auth0

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css'
-import React from 'react';
+
 import { UserProvider } from '@auth0/nextjs-auth0';
 
 export default function App({ Component, pageProps }) {
--- pages/index.js
+++ pages/index.js
@@ -6,27 +6,8 @@ import Footer from '../components/Footer'
 import { useState } from 'react';
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { useUser } from '@auth0/nextjs-auth0';
-
-export default function Index() {
-  const { user, error, isLoading } = useUser();
-
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>{error.message}</div>;
-
-  if (user) {
-    return (
-      <div>
-        Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
-      </div>
-    );
-  }
-
-  return <a href="/api/auth/login">Login</a>;
-}
 
 export default function Home() {
-
 
   const [loginInfo, setLoginInfo] = useState({});
   // sets loginInfo to a object that contains the users username and password. back end authentication needed. 
