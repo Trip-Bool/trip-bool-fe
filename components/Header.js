3202847bb8662b703b654a43205f929be0a5047a
Cole Gibbs <colegibbs0@gmail.com>
2022-06-22 14:23:53 -0700

    Restructured header and syle

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,9 @@
 
 export default function Header({ username }) {
   return(
-    <div className="flex justify-start justify-between ba bg-amber-100">
-      <h2>Welcome {username || ""}</h2>
-      <h1 className="text-3xl p-5">Tripin</h1>
-      <img></img>
+    <div className="flex justify-start justify-between bg-cyan-100 border-b-2 ">
+      <h1 className="text-3xl p-3">Tripin</h1>
+      <h2 className="mx-3">Welcome {username || ""}</h2>
     </div>
   )
 }
