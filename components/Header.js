72ed539cb325adde13ffc2ae1fd29edd06fd56ba
Jacob Choi <jacob.j.choi@gmail.com>
2022-06-22 14:28:51 -0700

    completed and working version

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,12 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
 
 export default function Header({ username }) {
   return(
     <>
     <h2>Welcome {username || ""}</h2>
     <h1>Tripin</h1>
+    <a href="/api/auth/login">Login</a>
+    <a href="/api/auth/logout">Logout</a>
     </>
   )
 }
