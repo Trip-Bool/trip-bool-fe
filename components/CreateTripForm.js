2dda2511e94a03f4bbd1988f058ca1b2c81675ea
Cole Gibbs <colegibbs0@gmail.com>
2022-06-22 14:23:27 -0700

    styling for create form

--- components/CreateTripForm.js
+++ components/CreateTripForm.js
@@ -13,7 +13,7 @@ export default function CreateTripForm({ handleTripCreation }) {
     <form className="w-4/5 m-auto my-10 rounded-3xl shadow-2xl bg-cyan-500"onSubmit={createTrip}>
     <fieldset className="flex flex-col p-5 space-y-6">
       <legend className="pt-5 m-auto text-2xl">Create A Trip</legend>
-      <label htmlFor="location">Destination</label>
+      <label className="border-t-2" htmlFor="location">Destination</label>
       <input className="text-xl rounded-md" type="text" name="location" />
       <label htmlFor="startDate">Start Date</label>
       <input className="text-xl rounded-md" type="date" name="startDate" />
