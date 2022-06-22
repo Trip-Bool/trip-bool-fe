0b78b50ea08fa4271cbb6c5512f8daef85b8c80f
Cole Gibbs <colegibbs0@gmail.com>
2022-06-22 11:38:22 -0700

    Header styling

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,10 @@
 
 export default function Header({ username }) {
   return(
-    <>
-    <h2 className="text-3xl font-bold underline">Welcome {username || ""}</h2>
-    <h1>Tripin</h1>
-    </>
+    <div className="flex justify-start justify-between ba bg-amber-100">
+      <h2>Welcome {username || ""}</h2>
+      <h1 className="text-3xl p-5">Tripin</h1>
+      <img></img>
+    </div>
   )
 }
